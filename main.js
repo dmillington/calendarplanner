1a6d02b38565afe0bf739a2353c4cfebb3d0d10a
Daniel Millington <danielsmillington@gmail.com>
2021-02-02 23:41:58 +0000

    simplify test setup
    remove index.pug and directly render an index.html file since we don't actually use template features

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.set("view engine", "pug");
-app.set("views", "./views");
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.sendFile("index.html", { root: __dirname })
 });
 
 const server = app.listen(port);
