4e438bd3824caa6281051f7c4a9596ac3a1ad7fd
Daniel Millington <danielsmillington@gmail.com>
2021-03-15 01:33:59 +0000

    main.js: local requests to work like github pages

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use(express.static('public'));
+app.use(express.static('docs/'));
 
 app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: __dirname })
+    res.sendFile("index.html", { root: __dirname  + '/docs/'})
+});
+
+app.get("/app", (req, res) => {
+    res.sendFile("app.html", { root: __dirname + '/docs/'})
+});
+
+app.get("/privacypolicy", (req, res) => {
+    res.sendFile("privacypolicy.html", { root: __dirname + '/docs/'})
 });
 
 const server = app.listen(port);
